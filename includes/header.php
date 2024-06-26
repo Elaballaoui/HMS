7b025d4bc709598482bdc84e12c28b9d49e831c7
Elaballaoui <mustapha.elaballaouii@gmail.com>
2024-06-26 20:53:18 +0100

    add assest and includes

---
+++ includes/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="assets/css/animations.css" />
+    <link rel="stylesheet" href="assets/css/main.css" />
+    <link rel="stylesheet" href="assets/css/index.css" />
+    <link rel="icon" href="img/hospital-logo.png" />
+    <title>eDoc</title>
+    <style>
+        table {
+            animation: transitionIn-Y-bottom 0.5s;
+        }
+    </style>
+</head>
+<body>
